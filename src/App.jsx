775081bb39c881eaebbb27801e46b97dc0ba308d
Nikimad <PybePoiD@gmail.com>
2023-07-05 17:10:39 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
-const App = () => {};
+const App = () => {
+    const getData = async () => {
+        const res = await fetch("/api/server/boards");
+        console.log(await res.json());
+    };
+    
+    const postData = () => {
+        fetch("/api/server/boards", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8",
+            },
+            body: JSON.stringify({ createdAt: Date.now() })
+        });
+    };
+
+    return (
+      <>
+        <button onClick={getData}>Get Data</button>
+        <button onClick={postData}>Post Data</button>
+      </>
+    );
+};
 
 export default App;
