43b9f048f518d74f565376db6518f5693b490861
Nikimad <PybePoiD@gmail.com>
2023-07-07 14:17:23 +0300

    Update

--- api/server.js
+++ api/server.js
@@ -5,8 +5,13 @@ export const server = jsonServer.create();
 
 const db = JSON.parse(fs.readFileSync("db.json"));
 const router = jsonServer.router(db);
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 server.use(router);
 server.listen(3000);
