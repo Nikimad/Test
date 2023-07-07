9fda5b1d7d998add8c2019b8afeea66ccd064adf
Nikimad <PybePoiD@gmail.com>
2023-07-07 14:04:40 +0300

    Update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ import jsonServer from "json-server";
 import fs from "fs";
 
 export const server = jsonServer.create();
+
 const db = JSON.parse(fs.readFileSync("db.json"));
 const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
 server.use(router);
 server.listen(3000);
