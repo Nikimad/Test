41a7a55b7c7725ff4a64df3a8daebd08cecd3603
Nikimad <PybePoiD@gmail.com>
2023-07-07 14:18:55 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 const App = () => {
     const getData = async () => {
-        const res = await fetch("api/server/boards");
+        const res = await fetch("api/boards");
         console.log(await res.json());
     };
     
     const postData = () => {
-        fetch("api/server/boards", {
+        fetch("api/boards", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json; charset=UTF-8",
